926079b733e15a9dbc5b054bf59a45dffdc7601d
Hayet Brabra <37178100+hayo03@users.noreply.github.com>
2021-05-03 16:40:45 +0200

    Add files via upload

--- webhook_service/webhook.py
+++ webhook_service/webhook.py
@@ -1,30 +1,45 @@
+#### minimal set of required modules
 import json
-from flask import request, jsonify, make_response
 from flask import Flask
-from flask import Response
+from flask import Response, request
 import requests
 
+
+###create a web app using Flask
 app = Flask(__name__)
 
 
+
+### Define a Route 
 @app.route('/my_webhook', methods=['POST'])
+
+### Define the function that will be executed when the associated route is called
 def post_webhook_dialogflow():
+#1) Getting information from dialogflow agent request 
     body = request.get_json(silent=True)
-    session_id = body['detectIntentResponseId']
-    print(session_id)
-    #The tag used to identify which fulfillment is being called.
+#Get tag used to identify which fulfillment is being called.
     fulfillment = body['fulfillmentInfo']['tag']
-    slots = []
+#Get parameters that are required to handle the desired action
+    prameters = []
     for key, value in body['sessionInfo']['parameters'].items():
-        slots.append({'name':key,'value':value})
-       
-    print (slots)
-    # msg = 'hi'
-    msg = invoke_api(fulfillment, slots)
-    return answer_webhook(msg, session_id)
+         prameters.append({'name':key,'value':value})
 
+#2) Execute action
+    msg = invoke_action(fulfillment,  prameters)
+#3) provide a webhook Response to the Dialogflow Agent
+    WebhookResponse=answer_webhook(msg)
+    return WebhookResponse
 
-def answer_webhook(msg, session_id):
+### Exploit parameters and incorporate them in the text response   
+def invoke_action(fulfillment,  prameters):
+
+    if fulfillment == "GetWeather_fulfillment":
+        city=str( prameters[0]['value'])
+        msg="There are overcast clouds in "+city
+        return msg
+
+#### Processes the webhook answer which should follow a particular JSON format
+def answer_webhook(msg):
     message= {"fulfillment_response": {
       
         "messages": [
@@ -37,28 +52,6 @@ def answer_webhook(msg, session_id):
     }
     }
     return Response(json.dumps(message), 200, mimetype='application/json')
-#get API credential given API name and attribute 
-def getAPI_credential(api_name, api_attribute):
-    data = json.load (open('API_credentials.json',))
-    return data[api_name][api_attribute]
-
-def invoke_api(fulfillment, slots):
-    print("\n\n\n\n\n=========> CALL API ",fulfillment)
-    if fulfillment == "GetWeather_fulfillment":
-        for slot in slots:
-             if slot['name']=="city":
-                 q=str(slot['value'])
-        appid=getAPI_credential('api.openweathermap','appid')
-        url = 'http://api.openweathermap.org/data/2.5/weather?q='+q+'&appid='+appid
-        result = requests.get(url)
-        jsonResult = result.json()
-        if result.status_code == 200:
-            weatherCondition = jsonResult['weather'][0]['description']
-            reply = "There is {} in there.".format(weatherCondition)
-            print(reply)
-            return reply
-        else:
-            return "Something wrong with the API."
-
+### Run a webhook on localhost
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8081, debug=True)
